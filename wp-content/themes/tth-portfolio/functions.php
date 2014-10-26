4f78ed332e1a337647e57706043e7cc435f9e2a9
Spenser Hale <spenserhale@outlook.com>
2014-10-25 19:06:23 -0700

    -added theme support 'menus' -registered primary menu

--- wp-content/themes/tth-portfolio/functions.php
+++ wp-content/themes/tth-portfolio/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+
+	register_nav_menus(
+		array(
+			'primary-menu' 	=> __( 'Primary Menu' )
+		)
+	);
+
+}
+add_action( 'init', 'register_theme_menus' );
+
 function wpt_theme_styles() {
 	wp_enqueue_style('foundation_css', get_template_directory_uri() . '/css/foundation.css');
 	//wp_enqueue_style('normalize_css', get_template_directory_uri() . '/css/normalize.css');
@@ -7,7 +20,6 @@ function wpt_theme_styles() {
 	wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css');
 
 }
-
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles');
 
 function wpt_theme_scripts() {
@@ -15,5 +27,4 @@ function wpt_theme_scripts() {
 	wp_enqueue_script('foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true);
 	wp_enqueue_script('main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true);
 }
-
 add_action( 'wp_enqueue_scripts', 'wpt_theme_scripts');
